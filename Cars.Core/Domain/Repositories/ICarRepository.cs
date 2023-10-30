b5b11dc2a83a740e324af581292d3effa826ba6d
Hikmət Həsənzadə <hikmet.hesenzade01@icloud.com>
2023-10-30 22:50:06 +0400

    added repositories

---
+++ Cars.Core/Domain/Repositories/ICarRepository.cs
@@ -0,0 +1,20 @@
+﻿using Cars.Core.Domain.Entites;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cars.Core.Domain.Repositories
+{
+    public class ICarRepository
+    {
+        void Add(Car car);
+        void Update(Car car);
+        void Delete(int id );
+
+        Car Get(int id);
+        List<Car> Get();
+
+    }
+}
---
+++ Cars.Core/Domain/Repositories/IMarkRepository.cs
@@ -0,0 +1,19 @@
+﻿using Cars.Core.Domain.Entites;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cars.Core.Domain.Repositories
+{
+    public class IMarkRepository
+    {
+        void Add(Mark mark);
+        void Update(Mark mark);
+        void Delete(int id);
+
+        Mark Get(int id);
+        List<Mark> Get();
+    }
+}
